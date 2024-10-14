6157f48a7a0958e0127cfd60a33b8a93c7ed1c2a
Shivam Wallu <141025670+ShivamWallu@users.noreply.github.com>
2024-10-14 15:35:22 +0530

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -56,7 +56,7 @@
     data() {
       return {
        // Set the URL of your image here
-        imageUrl: 'https://media.licdn.com/dms/image/v2/D5603AQGzl__BC3KtxQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1719769871531?e=1734566400&v=beta&t=vFfC6bCNRJC0Y7_QBrvt0wlGd8gdZq3N2DU66ME8n3M',
+        // imageUrl: 'https://media.licdn.com/dms/image/v2/D5603AQGzl__BC3KtxQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1719769871531?e=1734566400&v=beta&t=vFfC6bCNRJC0Y7_QBrvt0wlGd8gdZq3N2DU66ME8n3M',
       };
     },
   };
